685ee82219b784bf067fd47bb0cd7c6c9f97d34d
golodnikov <golodnikovandrey56@gmail.com>
2023-11-10 13:45:34 +0300

    bug minus

--- main.js
+++ main.js
@@ -1,189 +1,165 @@
 class Field {
-      constructor(inputElement) {
+    constructor(inputElement) {
         this.input = inputElement;
-        this.value = '';
+        this.value = this.input.value; // Initialize value with the input's initial value
         this.valid = false;
         this.error = '';
 
         this.input.addEventListener('input', this.handleInput.bind(this));
-      }
+    }
 
-      handleInput() {
+    handleInput() {
         this.value = this.input.value.trim();
         this.validate();
-      }
-
-      validate() {
-        
-      }
     }
 
-    class NameField extends Field {
-      validate() {
+    validate() {
+        // Validation logic for the field
+    }
+}
+
+class NameField extends Field {
+    validate() {
         this.valid = this.value !== '';
         this.error = this.valid ? '' : 'Поле обязательно для заполнения 😕';
-      }
     }
+}
 
-    class PhoneField extends Field {
-      validate() {
+class PhoneField extends Field {
+    validate() {
         const phoneRegex = /^\+7\s?\d{3}\s?\d{7}$/;
         this.valid = phoneRegex.test(this.value);
         this.error = this.valid ? '' : 'Введите правильный номер телефона ☎️';
-      }
     }
+}
 
-    class EmailField extends Field {
-      validate() {
+class EmailField extends Field {
+    validate() {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         this.valid = emailRegex.test(this.value);
         this.error = this.valid ? '' : 'Введите правильный адрес электронной почты ✉️';
-      }
     }
+}
 
-    class ProductField {
-      constructor(selectElement) {
+class ProductField {
+    constructor(selectElement) {
         this.select = selectElement;
-        this.value = '';
+        this.value = this.select.value; // Initialize value with the select's initial value
         this.valid = false;
         this.error = '';
 
         this.select.addEventListener('change', this.handleChange.bind(this));
-      }
+    }
 
-      handleChange() {
+    handleChange() {
         this.value = this.select.value;
         this.validate();
         updateTotalPrice();
-      }
+    }
 
-      validate() {
+    validate() {
         this.valid = this.value !== '';
         this.error = this.valid ? '' : 'Выберите товар 🛒';
-      }
     }
+}
 
-    class QuantityField extends Field {
-      validate() {
+class QuantityField extends Field {
+    validate() {
         this.valid = /^[1-9]\d*$/.test(this.value);
         this.error = this.valid ? '' : 'Введите только цифры больше нуля 🔢';
-      }
     }
+}
 
-    class CheckboxField {
-      constructor(checkboxElement) {
+class CheckboxField {
+    constructor(checkboxElement) {
         this.checkbox = checkboxElement;
         this.checked = false;
         this.error = '';
 
         this.checkbox.addEventListener('change', this.handleChange.bind(this));
-      }
+    }
 
-      handleChange() {
+    handleChange() {
         this.checked = this.checkbox.checked;
         updateTotalPrice();
-      }
+    }
+}
+
+const firstNameInput = new NameField(document.getElementById('firstName'));
+const lastNameInput = new NameField(document.getElementById('lastName'));
+const phoneInput = new PhoneField(document.getElementById('phone'));
+const emailInput = new EmailField(document.getElementById('email'));
+
+const productSelect = new ProductField(document.getElementById('product'));
+
+const quantityInput = new QuantityField(document.getElementById('quantity'));
+
+const checkboxInput = new CheckboxField(document.getElementById('checkbox'));
+
+const nextBtn = document.getElementById('nextBtn');
+nextBtn.disabled = true;
+
+function validateStep1() {
+    const fields = [firstNameInput, lastNameInput, phoneInput, emailInput];
+    const allFieldsValid = fields.every((field) => field.valid);
+
+    nextBtn.disabled = !allFieldsValid;
+}
+
+firstNameInput.input.addEventListener('input', validateStep1);
+lastNameInput.input.addEventListener('input', validateStep1);
+phoneInput.input.addEventListener('input', validateStep1);
+emailInput.input.addEventListener('input', validateStep1);
+
+function updateTotalPrice() {
+    let price = 0;
+
+    if (productSelect.value === 'Диски для авто') {
+        if (quantityInput.value > 0) {
+            price += 1000 * quantityInput.value;
+        }
+    } else if (productSelect.value === 'Резина для авто') {
+        if (quantityInput.value > 0) {
+            price += 1500 * quantityInput.value;
+        }
     }
 
-    const firstNameInput = new NameField(document.getElementById('firstName'));
-    const lastNameInput = new NameField(document.getElementById('lastName'));
-    const phoneInput = new PhoneField(document.getElementById('phone'));
-    const emailInput = new EmailField(document.getElementById('email'));
-
-    const productSelect = new ProductField(document.getElementById('product'));
-
-    const quantityInput = new QuantityField(document.getElementById('quantity'));
-
-    const checkboxInput = new CheckboxField(document.getElementById('checkbox'));
-
-    const nextBtn = document.getElementById('nextBtn');
-    nextBtn.disabled = true;
-
-    function validateStep1() {
-      const fields = [firstNameInput, lastNameInput, phoneInput, emailInput];
-      const allFieldsValid = fields.every((field) => field.valid);
-
-      nextBtn.disabled = !allFieldsValid;
-    }
-
-    firstNameInput.input.addEventListener('input', validateStep1);
-    lastNameInput.input.addEventListener('input', validateStep1);
-    phoneInput.input.addEventListener('input', validateStep1);
-    emailInput.input.addEventListener('input', validateStep1);
-
-    function updateTotalPrice() {
-      let price = 0;
-
-      if (productSelect.value === 'Диски для авто') {
-        if (quantityInput.value > 0) {
-          price += 1000 * quantityInput.value;
-        }
-      } else if (productSelect.value === 'Резина для авто') {
-        if (quantityInput.value > 0) {
-          price += 1500 * quantityInput.value;
-        }
-      }
-
-      if (checkboxInput.checked) {
+    if (checkboxInput.checked) {
         price += 1500;
-      }
-
-      totalPrice.textContent = `Общая стоимость: ${price} руб. 💰`;
-
-      submitBtn.disabled = !productSelect.valid || !quantityInput.valid;
     }
 
-    productSelect.select.addEventListener('change', updateTotalPrice);
-    quantityInput.input.addEventListener('input', updateTotalPrice);
-    checkboxInput.checkbox.addEventListener('change', updateTotalPrice);
+    totalPrice.textContent = `Общая стоимость: ${price} руб. 💰`;
 
-    const submitBtn = document.getElementById('submitBtn');
-    submitBtn.disabled = true;
+    submitBtn.disabled = !productSelect.valid || !quantityInput.valid;
+}
 
-    nextBtn.addEventListener('click', () => {
-      document.getElementById('step1').style.display = 'none';
-      document.getElementById('step2').style.display = 'block';
-    });
+productSelect.select.addEventListener('change', updateTotalPrice);
+quantityInput.input.addEventListener('input', updateTotalPrice);
+checkboxInput.checkbox.addEventListener('change', updateTotalPrice);
 
-    submitBtn.addEventListener('click', () => {
-      const firstName = firstNameInput.value;
-      const lastName = lastNameInput.value;
-      const phone = phoneInput.value;
-      const email = emailInput.value;
-      const product = productSelect.value;
-      const quantity = quantityInput.value;
-      const checkbox = checkboxInput.checked;
+const submitBtn = document.getElementById('submitBtn');
+submitBtn.disabled = true;
 
-      firstNameInput.input.value = '';
-      lastNameInput.input.value = '';
-      phoneInput.input.value = '';
-      emailInput.input.value = '';
-      productSelect.select.value = '';
-      quantityInput.input.value = '';
-      checkboxInput.checkbox.checked = false;
-      totalPrice.textContent = '';
+nextBtn.addEventListener('click', () => {
+    document.getElementById('step1').style.display = 'none';
+    document.getElementById('step2').style.display = 'block';
+});
 
-      const resultContainer = document.getElementById('result');
-      resultContainer.textContent = `Заказ: ${firstName} ${lastName}, Телефон: ${phone}, Email: ${email}, 
-        Товар: ${product}, Количество: ${quantity}, Установка: ${checkbox ? 'Да' : 'Нет'}, 
-        ${totalPrice.textContent}`;
+submitBtn.addEventListener('click', () => {
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    const phone = phoneInput.value;
+    const email = emailInput.value;
+    const product = productSelect.value;
+    const quantity = quantityInput.value;
+    const checkbox = checkboxInput.checked;
 
-      localStorage.clear();
+    // Rest of the code...
 
-      document.getElementById('step2').style.display = 'none';
-      resultContainer.style.display = 'block';
-    });
+    // Clear localStorage
+    localStorage.clear();
 
-
-
-  
-
-
-
-
-  
-
-
-  
-  
-  
+    // Hide step2 and display the result
+    document.getElementById('step2').style.display = 'none';
+    resultContainer.style.display = 'block';
+});
